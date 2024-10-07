921ee5151b534924f8ffc0311413d61c31ed8ac9
Shina Qin <bqshina@gmail.com>
2024-10-07 14:17:10 +1100

    adjust the navbar

--- portfolio-app/app/components/NavBar/NavBar.tsx
+++ portfolio-app/app/components/NavBar/NavBar.tsx
@@ -4,14 +4,14 @@ import styles from "./NavBar.module.css";
 
 const NarBar = () => {
   return (
-    <div className="navbar bg-base-100 px-52">
+    <div className={`navbar bg-base-100 ${styles.navbar}`}>
       <div className="flex-1">
         <Link href="#" className={styles.logo}>
-          Shina Qin
+          SHINA QIN
         </Link>
       </div>
       <div className="flex-none">
-        <ul className="menu menu-horizontal px-1">
+        <ul className={`menu menu-horizontal px-2 ${styles.navLinks}`}>
           <li>
             <Link href="#">Work</Link>
           </li>
@@ -25,7 +25,12 @@ const NarBar = () => {
             <Link href="#">Experience</Link>
           </li>
           <li>
-            <Link href="#">Contact</Link>
+            <Link
+              href="#"
+              className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-base px-8 py-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+            >
+              Contact
+            </Link>
           </li>
         </ul>
       </div>
