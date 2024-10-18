0d16cb74137461dc35ba9b9d02d353035ea9409c
Shina Qin <bqshina@gmail.com>
2024-10-19 10:31:27 +1100

    clear code

--- portfolio-app/app/page.tsx
+++ portfolio-app/app/page.tsx
@@ -1,18 +1,14 @@
 import AboutMe from "./components/About";
 import Heading from "./components/Heading";
-import Posts from "./components/Posts";
 import Projects from "./components/Projects/Projects";
 import Skills from "./components/Skills";
-import Tools from "./components/Tools";
 
 export default function Home() {
   return (
     <main>
       <Heading />
       <Projects />
-      {/* <Tools /> */}
       <Skills />
-      {/* <Posts /> */}
       <AboutMe />
     </main>
   );
