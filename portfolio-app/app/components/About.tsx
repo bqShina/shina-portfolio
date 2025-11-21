6a43920bfe13eda0927f1b3b6448ac687aa1a790
Shina Qin <bqshina@gmail.com>
2025-11-21 16:57:32 +1100

    update about section

--- portfolio-app/app/components/About.tsx
+++ portfolio-app/app/components/About.tsx
@@ -18,12 +18,13 @@ const AboutMe = () => {
       <div className="lg:ml-20 ml-0 w-[100%] lg:w-[50%]">
         <h4 className="text-[28px] mb-3">Hi, I&apos;m Shina! 💻</h4>
         <p className="text-[#505050] leading-loose mb-10">
-          I&apos;m a recent graduate and Full-Stack Developer based in
-          Melbourne, Australia, with over a year of experience building
-          high-quality websites and products.
-          <br />
-          If you&apos;re looking for someone passionate about delivering
-          innovative solutions, feel free to reach out!
+          I&apos;m a Full-Stack Developer based in Melbourne.
+          <br />I specialize in building web applications using{" "}
+          <strong>React, Next.js, Node.js, and ASP.NET</strong>. With a strong
+          focus on creating user-friendly and scalable solutions, I enjoy
+          turning complex problems into simple, functional applications. I
+          thrive in collaborative teams and am always learning new tools and
+          technologies to improve my craft.
         </p>
         <div className="text-white bg-gray-800 px-8 py-5 rounded-full transition duration-200 hover:scale-95 inline-block">
           <Link href="/resume.pdf">Download my resume</Link>
